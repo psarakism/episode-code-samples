e64ea017b0a64c07dc37c0a6e4318c9f772bc6a8
Point-Free <pointfreeco@Stephens-MacBook-Pro.local>
2021-11-09 12:16:03 -0800

    wip

--- xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/SwiftUINavigationApp.swift
@@ -8,22 +8,30 @@ struct SwiftUINavigationApp: App {
     var editedKeyboard = keyboard
     editedKeyboard.name = "Bluetooth Keyboard"
     editedKeyboard.status = .inStock(quantity: 1000)
+    
+    let viewModel = AppViewModel(
+      inventoryViewModel: .init(
+        inventory: [
+          .init(item: keyboard),
+          .init(item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))),
+          .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
+          .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
+        ],
+        route: nil
+      ),
+      selectedTab: .one
+    )
 
     return WindowGroup {
-      ContentView(
-        viewModel: .init(
-          inventoryViewModel: .init(
-            inventory: [
-              .init(item: keyboard),
-              .init(item: Item(name: "Charger", color: .yellow, status: .inStock(quantity: 20))),
-              .init(item: Item(name: "Phone", color: .green, status: .outOfStock(isOnBackOrder: true))),
-              .init(item: Item(name: "Headphones", color: .green, status: .outOfStock(isOnBackOrder: false))),
-            ],
-            route: nil
-          ),
-          selectedTab: .one
+//      ContentView(
+      ToSwiftUI {
+        ContentViewController(
+          viewModel: viewModel
         )
-      )
+      }
+      .onOpenURL { url in
+        viewModel.open(url: url)
+      }
     }
   }
 }
