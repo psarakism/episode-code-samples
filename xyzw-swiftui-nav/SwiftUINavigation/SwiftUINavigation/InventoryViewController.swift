4900b59e1c624fbd724ae3f667bb2200be871469
Point-Free <pointfreeco@Stephens-MacBook-Pro.local>
2021-11-09 11:05:36 -0800

    wip

--- xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/InventoryViewController.swift
+++ xyzw-swiftui-nav/SwiftUINavigation/SwiftUINavigation/InventoryViewController.swift
@@ -2,6 +2,16 @@ import Combine
 import SwiftUI
 import UIKit
 
+extension ItemRowViewModel: Hashable {
+  static func == (lhs: ItemRowViewModel, rhs: ItemRowViewModel) -> Bool {
+    lhs === rhs
+  }
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(self.item)
+  }
+}
+
 class InventoryViewController: UIViewController {
   let viewModel: InventoryViewModel
   private var cancellables: Set<AnyCancellable> = []
@@ -27,8 +37,50 @@ class InventoryViewController: UIViewController {
       }
     )
     self.title = "Inventory"
+    
+    enum Section { case inventory }
+    
+    let cellRegistration = UICollectionView.CellRegistration<
+      ItemRowCellView, ItemRowViewModel
+    >.init { [unowned self] cell, indexPath, itemRowViewModel in
+      cell.bind(viewModel: itemRowViewModel, context: self)
+    }
+    
+    let collectionView = UICollectionView(
+      frame: .zero,
+      collectionViewLayout: UICollectionViewCompositionalLayout.list(using: .init(appearance: .insetGrouped))
+    )
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
 
+    let dataSource = UICollectionViewDiffableDataSource<Section, ItemRowViewModel>(
+      collectionView: collectionView
+    ) { collectionView, indexPath, itemRowViewModel in
+      collectionView.dequeueConfiguredReusableCell(
+        using: cellRegistration,
+        for: indexPath,
+        item: itemRowViewModel
+      )
+    }
+    collectionView.dataSource = dataSource
+    
+    self.view.addSubview(collectionView)
+    NSLayoutConstraint.activate([
+      collectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+      collectionView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+      collectionView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+    ])
+    
     // MARK: view model bindings
+    
+    self.viewModel.$inventory
+      .sink { inventory in
+        var snapshot = NSDiffableDataSourceSnapshot<Section, ItemRowViewModel>()
+        snapshot.appendSections([.inventory])
+        snapshot.appendItems(inventory.elements, toSection: .inventory)
+        dataSource.apply(snapshot, animatingDifferences: true)
+      }
+      .store(in: &self.cancellables)
 
     var presentedViewController: UIViewController?
 
@@ -75,7 +127,14 @@ struct InventoryViewController_Previews: PreviewProvider {
   static var previews: some View {
       ToSwiftUI {
         UINavigationController(
-          rootViewController: InventoryViewController(viewModel: .init())
+          rootViewController: InventoryViewController(
+            viewModel: .init(
+              inventory: [
+                .init(item: .init(name: "Keyboard", color: .red, status: .inStock(quantity: 1)))
+              ],
+              route: nil
+            )
+          )
         )
       }
   }
